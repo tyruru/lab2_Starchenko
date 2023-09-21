f456c94cefe444d6141700779197fae8ed19caf6
tyruru <littlespace12@gmail.com>
2023-09-21 13:19:52 +0300

    new

---
+++ lab_2_Starchenko.py
@@ -0,0 +1,22 @@
+print("Programming***Essentials***in...Python")
+
+print("\n",
+      "    *    \n",
+      "   * *   \n",
+      "  *   *  \n",
+      " *     * \n",
+      "***   ***\n",
+      "  *   *  \n",
+      "  *   *  \n",
+      "  *****  \n",)
+
+
+print("I'm student")
+
+
+print('"I\'m" \n""learning"" \n"""Python"""')
+
+a = 0o500;
+print (a)
+
+print (0o777)
