2b491604d7f284f34467e03fadc2045e36f2f716
tyruru <98100803+tyruru@users.noreply.github.com>
2023-10-18 23:04:08 +0300

    lab7

--- lab_7_Starchenko/lab_7_Starchenko.py
+++ lab_7_Starchenko/lab_7_Starchenko.py
@@ -1,11 +1,52 @@
+##################################
+
+korteg = (3,10,25,-1,0,9)
+
+n = 10
+newKorteg = ()
+
+for num in korteg:
+    if num < n:
+        newKorteg += (num,)
+
+print (newKorteg)
+
+##################################
+
+korteg =("apple", "souce", "Miami")
+str = ', '.join(korteg)
+
+print (str)
+
+##################################
+
+def printInfo(book):
+    print("author:", book["avtor"])
+    print("year:", book["year"])
+    print("pages:", book["pages"])
+
+
+books ={
+    "33Bogaturya" : {"avtor" : "Shevchenko", "year" : "1337", "pages" : 1337},
+    "Sherlock Holmes" : {"avtor" : "Hmelnickiy", "year" : "1978", "pages" : 10},
+    "1306" : {"avtor" : "1306", "year" : "1306", "pages" : 1306}
+}
+
+answer = input("Введіть бажану книгу: ")
+
+if answer in books:
+    book = books[answer]
+    printInfo(book)
+else:
+    print("Такой книги не существует!")
+
+##################################
+
 def printStudents(students):
-    for lastName in students:
-        adding = 0
-        counter = 0
-        for score in students[lastName]:
-            adding += score
-            counter += 1
-            print (lastName, adding / counter)
+    for lastName, info in students.items():
+         print(f"\nФамилия: {lastName}", 
+               f"\nИмя: {info['name']}",
+               f"\nКурс: {info['kurs']}")
 
 
 students = {}
@@ -23,13 +64,39 @@ while True:
     if kurs < 1:
         break
 
-    # if lastName in students:
-    students[lastName] = {"name " : name, "kurs" : kurs}
-    # else:
-    #     students[lastName] = {"name " : name, "kurs" : kurs}
+    if lastName in students:
+        students[lastName] = {"name" : name, "kurs" : kurs}
+    else:
+         students[lastName] = {"name" : name, "kurs" : kurs}
     
 
 printStudents(students)
 
-# for student in students["1"]:
-#     print(student["lastName"])
+
+##################################
+
+def addNumber():
+    name = input("Enter the contact name: ")
+    if (name == ""):
+        print("Wrong name!")
+        return
+    phoneNumber = input("Enter the number: ")
+    if (phoneNumber == ""):
+        print("WrongNumber!")
+        return
+    global contacts
+    contacts[name] = {"phoneNumber" : phoneNumber}
+
+def printContacts():
+    global contacts
+    for name, num in contacts.items():
+        print(f"Name: {name} \nPhone number: {num['phoneNumber']}\n")
+
+
+contacts = {}
+addNumber()
+addNumber()
+printContacts()
+
+
+##################################
